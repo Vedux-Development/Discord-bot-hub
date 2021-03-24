aaef7b3b2104e9d2ea38e3ad5fd7bf74027b2f3e
Crimson <68981140+ducksquaddd@users.noreply.github.com>
2021-03-24 07:09:02 -0700

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -3,134 +3,8 @@
 const inquirer = require('inquirer');
 const shell = require('shelljs');
 const fs = require('fs-extra');
-const managejs1 = `
-const readline = require('readline');
-const shell = require("shelljs")
-const prompt = require('prompt-sync')();
-
-shell.exec("clear")
-console.log("You can type help to find out the commands")
-
-function replDemo() {
-  return new Promise(function(resolve, reject) {
-	let rl = readline.createInterface(process.stdin, process.stdout)
-	rl.setPrompt('ready> ')
-	rl.prompt();
-	rl.on('line', function(line) {
-	  if (line === "exit" || line === "quit" || line == 'q') {
-		rl.close()
-		return // bail here, so rl.prompt() isn't called again
-	  }
-
-	  if (line === "help" || line === '?') {
-		console.log(\`[vsc] Open vsc \n[start] Start The Bot \n[update] Update Needed Packages \n[install] Install Packages \n[needed] Run this to install needed packages \n [exit] this will exit this script\`)
-	  } else if (line === "start") {
-		shell.exec("node index.js")
-		rl.close()
-	  } else if (line === "vsc") {
-		shell.exec("code .")
-	  } else if (line === "update"){
-		console.log("Updating packages")
-		shell.exec("npm i")
-	  } else if (line === "install"){
-		console.log("What package do you wish to install")
-		var package = prompt()
-		shell.exec(\`npm i \${package}\`)
-		console.log("Installed fs")
-	  } else if (line === "needed"){
-		console.log("Installing the packages")
-		shell.exec("npm i discord.js")
-		console.log("Installed Discord.js")
-		shell.exec("npm i fs")
-		console.log("Installed fs")
-	  } else {
-		console.log(\`Unknown command: "$\{line}"\`)
-	  }
-	  rl.prompt()
-
-	}).on('close',function(){
-	  console.log('bye')
-	  resolve(42) // this is the final result of the function
-	});
-  })
-}
-
-async function run() {
-  try {
-	let replResult = await replDemo()
-	console.log('repl result:', replResult)
-
-  } catch(e) {
-	console.log('failed:', e)
-  }
-}
-
-run()
-`;
-const managejs2 = `
-const readline = require('readline');
-const shell = require("shelljs")
-const prompt = require('prompt-sync')();
-
-shell.exec("clear")
-console.log("You can type help to find out the commands")
-
-function replDemo() {
-  return new Promise(function(resolve, reject) {
-	let rl = readline.createInterface(process.stdin, process.stdout)
-	rl.setPrompt('ready> ')
-	rl.prompt();
-	rl.on('line', function(line) {
-	  if (line === "exit" || line === "quit" || line == 'q') {
-		rl.close()
-		return // bail here, so rl.prompt() isn't called again
-	  }
-
-	  if (line === "help" || line === '?') {
-		console.log(\`[vsc] Open vsc \n[start] Start The Bot \n[update] Update Needed Packages \n[install] Install Packages \n[needed] Run this to install needed packages \n [exit] this will exit this script\`)
-	  } else if (line === "start") {
-		shell.exec("node index.js")
-		rl.close()
-	  } else if (line === "vsc") {
-		shell.exec("code .")
-	  } else if (line === "update"){
-		console.log("Updating packages")
-		shell.exec("npm i")
-	  } else if (line === "install"){
-		console.log("What package do you wish to install")
-		var package = prompt()
-		shell.exec(\`npm i \${package}\`)
-		console.log("Installed fs")
-	  } else if (line === "needed"){
-		console.log("Installing the packages")
-		shell.exec("npm i discord.js")
-		console.log("Installed Discord.js")
-		shell.exec("npm i fs")
-		console.log("Installed fs")
-	  } else {
-		console.log(\`Unknown command: "$\{line}"\`)
-	  }
-	  rl.prompt()
-
-	}).on('close',function(){
-	  console.log('bye')
-	  resolve(42) // this is the final result of the function
-	});
-  })
-}
-
-async function run() {
-  try {
-	let replResult = await replDemo()
-	console.log('repl result:', replResult)
-
-  } catch(e) {
-	console.log('failed:', e)
-  }
-}
-
-run()
-`;
+const cmdhandler = require("./templates/cmdhandler")
+const errormessages = require("./templates/errors.json")
 
 
 // console.log(process.cwd());
@@ -142,24 +16,10 @@ if (!fs.existsSync(dir)) {
 shell.cd(dir);
 
 const file = './managebots.js';
-const managefile = `
 
-const prompt = require("prompt-sync")();
-var shell = require("shelljs");
-shell.config.verbose = false;
+fs.writeFileSync(file, cmdhandler.managefile);
 
-shell.exec('clear')
-shell.exec(
-  "ls -I node_modules -I start.js -I managebots.js -I package.json -I package-lock.json -I app.js -I README.md -I LICENSE"
-);
-const cdfile = prompt("What bot would you like to manage: ");
 
-shell.cd(\`\${cdfile}\`);
-
-require("child_process").fork("manage.js");
-
-`;
-fs.writeFileSync(file, managefile);
 
 inquirer.prompt([{
 	type: 'list',
@@ -180,6 +40,7 @@ inquirer.prompt([{
 				.then(function(answer) {
 					if (answer.language == 'discord.py') {
 						console.log('Sorry this feature is not out yet');
+						console.log('But you can always make a pull request with this feature')
 					}
 					if (answer.language == 'discord.js') {
 						inquirer.prompt([{
@@ -208,9 +69,8 @@ inquirer.prompt([{
 											])
 												.then(function(answer) {
 													console.log('Installing the repository');
-													shell.exec(`git clone ${answer.repo} ${botDir}`);
-													fs.writeFile(`./${botDir}/manage.js`, managejs2, (err) => {
-														// throws an error, you could also catch it here
+													shell.exec(`git clone ${answer.repo} ${botDir}`)
+													fs.writeFile(`./${botDir}/manage.js`, cmdhandler.manage2, (err) => {
 														
 													});
 													inquirer.prompt([{
@@ -241,7 +101,7 @@ inquirer.prompt([{
 											}
 
 
-											fs.writeFile(`./${botDir}/manage.js`, managejs2, (err) => {
+											fs.writeFile(`./${botDir}/manage.js`, cmdhandler.manage2, (err) => {
 											});
 											inquirer.prompt([
 												{
@@ -307,67 +167,14 @@ inquirer.prompt([{
 											writeStream.write('} \n');
 											writeStream.end();
 
-											const index = `
-      let Discord = require("discord.js");
-      let fs = require("fs")
-      let client = new Discord.Client();
-      client.commands = new Discord.Collection();
-      const config = require("./config.json")
-      client.token = config.token;
-      client.status = config.status;
-      fs.readdir("./commands/", (err, files) => {
-          if (err) console.log(err)
-          let jsfile = files.filter(f => f.split(".").pop() === "js")
-          if(jsfile.length <= 0){
-              console.log("No commands found...")
-              return;
-          }
-          jsfile.forEach((f, i) => {
-              let props = require(\`./commands/\${f}\`)
-              console.log(\`\${f} loaded\`)
-              client.commands.set(props.help.name, props);
-          })
-      })
-      client.on('message', async message => {
-          if(message.author.bot) return;
-          if(message.channel.type === 'dm') return;
-          let prefix = config.prefix;
-          let messageArray = message.content.split(" ")
-          let cmd = messageArray[0]
-          let args = messageArray.slice(1)
-          let commandfile = client.commands.get(cmd.slice(prefix.length));
-          if(commandfile) commandfile.run(client, message, args);
-      })
-      client.on("ready", () =>{
-          client.user.setPresence({
-              status: 'online',
-              activity: {
-                  name: config.status,
-                  type: 'WATCHING',
-                  url: 'https://www.twitch.tv/'
-              }
-          })
-       });
-      client.login(client.token)
-      
-`;
-											fs.writeFile(`./${botDir}/index.js`, index, (err) => {
+											fs.writeFile(`./${botDir}/index.js`, cmdhandler.index, (err) => {
 												// throws an error, you could also catch it here
 												if (err) throw err;
 
 												// success case, the file was saved
 											});
-											const command1 = `
-const Discord = require("discord.js")
-module.exports.run = async (client, message, args) => {
-    message.channel.send("Hellooooo")
-}
-module.exports.help = {
-    name: "hello"
-}
-`;
 
-											fs.writeFile(`./${botDir}/${commandsf}/hello.js`, command1, (err) => {
+											fs.writeFile(`./${botDir}/${commandsf}/hello.js`, cmdhandler.command1, (err) => {
 												// throws an error, you could also catch it here
 												if (err) throw err;
 
@@ -375,9 +182,9 @@ module.exports.help = {
 											});
 
 
-											fs.writeFile(`./${botDir}/manage.js`, managejs1, (err) => {
+											fs.writeFile(`./${botDir}/manage.js`, cmdhandler.manage1, (err) => {
 												// throws an error, you could also catch it here
-									
+												
 											});
 											inquirer.prompt([{
 												type: 'list',
@@ -400,6 +207,24 @@ module.exports.help = {
 				});
 		}
 		if (answer.startanswer == 'Update DBH') {
-			console.log('Sorry this feature is currently not out!');
+			console.log("Updating DBH")
+			inquirer.prompt([{
+				type: 'list',
+				name: 'os',
+				message: 'What is your OS',
+				choices: ['Windows','Linux/Mac']
+			}])
+			.then(function(answer){
+				if (answer.os == 'Windows'){
+					console.log("Updating DBH, This may take awhile")
+					shell.exec('npm i -g discord-bot-hub')
+					console.log("Finished install")
+				}
+				else if (answer.os == 'Linux/Mac'){
+					console.log("Updating DBH, This may take awhile")
+					shell.exec('sudo npm i -g discord-bot-hub')
+					console.log("Finished updating")
+				}
+			})
 		}
 	});
