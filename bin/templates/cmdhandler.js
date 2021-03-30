f7346d7bf89d9934318cae3a7d13a64b2fa4d6c1
Crimson <68981140+ducksquaddd@users.noreply.github.com>
2021-03-30 14:39:18 -0700

    Update cmdhandler.js

--- bin/templates/cmdhandler.js
+++ bin/templates/cmdhandler.js
@@ -1,230 +1,199 @@
-#!/usr/bin/env node
+exports.manage1 = `const readline = require('readline');
+const shell = require("shelljs")
+const prompt = require('prompt-sync')();
 
-const inquirer = require('inquirer');
-const shell = require('shelljs');
-const fs = require('fs-extra');
-const cmdhandler = require("./templates/cmdhandler")
-const errormessages = require("./templates/errors.json")
+shell.exec("clear")
+console.log("You can type help to find out the commands")
 
+function replDemo() {
+  return new Promise(function(resolve, reject) {
+	let rl = readline.createInterface(process.stdin, process.stdout)
+	rl.setPrompt('ready> ')
+	rl.prompt();
+	rl.on('line', function(line) {
+	  if (line === "exit" || line === "quit" || line == 'q') {
+		rl.close()
+		return // bail here, so rl.prompt() isn't called again
+	  }
 
-// console.log(process.cwd());
+	  if (line === "help" || line === '?') {
+		console.log(\`[vsc] Open vsc \n[start] Start The Bot \n[update] Update Needed Packages \n[install] Install Packages \n[needed] Run this to install needed packages \n [exit] this will exit this script\`)
+	  } else if (line === "start") {
+		shell.exec("node index.js")
+		rl.close()
+	  } else if (line === "vsc") {
+		shell.exec("code .")
+	  } else if (line === "update"){
+		console.log("Updating packages")
+		shell.exec("npm i")
+	  } else if (line === "install"){
+		console.log("What package do you wish to install")
+		var package = prompt()
+		shell.exec(\`npm i \${package}\`)
+		console.log("Installed fs")
+	  } else if (line === "needed"){
+		console.log("Installing the packages")
+		shell.exec("npm i discord.js")
+		console.log("Installed Discord.js")
+		shell.exec("npm i fs")
+		console.log("Installed fs")
+	  } else {
+		console.log(\`Unknown command: "$\{line}"\`)
+	  }
+	  rl.prompt()
 
-const dir = './discord-bot-hub';
-if (!fs.existsSync(dir)) {
-	fs.mkdirSync(dir);
-}
-shell.cd(dir);
-
-const file = './managebots.js';
-
-fs.writeFileSync(file, cmdhandler.managefile);
-
-
-
-inquirer.prompt([{
-	type: 'list',
-	name: 'startanswer',
-	message: 'Select what you wanna do!',
-	choices: ['Manage bots?', 'Setup a bot?', 'Update DBH'],
-}])
-	.then(function(answer) {
-		if (answer.startanswer == 'Manage bots?') {
-			require('child_process').fork('managebots.js');
-		} else if (answer.startanswer == 'Setup a bot?') {
-			inquirer.prompt([{
-				type: 'list',
-				name: 'language',
-				message: 'Please select your language',
-				choices: ['discord.js', 'discord.py'],
-			}])
-				.then(function(answer) {
-					if (answer.language == 'discord.py') {
-						console.log('Sorry this feature is not out yet');
-						console.log('But you can always make a pull request with this feature')
-					}
-					if (answer.language == 'discord.js') {
-						inquirer.prompt([{
-							type: 'list',
-							name: 'djssetup',
-							message: 'What template do you want to use?',
-							choices: ['Basic bot with a command handler', 'Make your own bot', 'Install a github repo'],
-						}])
-							.then(function(answer) {
-								if (answer.djssetup == 'Install a github repo') {
-									inquirer.prompt([{
-										type: 'input',
-										name: 'dirname',
-										message: 'What do you want your bot dir to be?',
-										default: 'discord-bot',
-									}])
-										.then(function(answer) {
-											const botDir = `${answer.dirname}`;
-
-											inquirer.prompt([
-												{
-													type: 'input',
-													name: 'repo',
-													message: 'Git install link',
-												},
-											])
-												.then(function(answer) {
-													console.log('Installing the repository');
-													shell.exec(`git clone ${answer.repo} ${botDir}`)
-													fs.writeFile(`./${botDir}/manage.js`, cmdhandler.manage2, (err) => {
-														
-													});
-													inquirer.prompt([{
-														type: 'list',
-														name: 'managehub',
-														message: 'Would you like to go to manage hub, By going here you can manage your bot!',
-														choices: ['Yes', 'No']
-													}])
-														.then(function(answer) {
-															if (answer.managehub == 'Yes') {
-																require('child_process').fork('managebots.js');
-															}
-														});
-												});
-										});
-								}
-								if (answer.djssetup == 'Make your own bot') {
-									inquirer.prompt([{
-										type: 'input',
-										name: 'dirname',
-										message: 'What do you want your bot dir to be?',
-										default: 'discord-bot',
-									}])
-										.then(function(answer) {
-											const botDir = `${answer.dirname}`;
-											if (!fs.existsSync(botDir)) {
-												fs.mkdirSync(botDir);
-											}
-
-
-											fs.writeFile(`./${botDir}/manage.js`, cmdhandler.manage2, (err) => {
-											});
-											inquirer.prompt([
-												{
-												type: 'list',
-												name: 'managehub22',
-												message: 'Would you like to go to manage hub, By going here you can manage your bot!',
-												choices: ["Yes","No"],
-												}
-											])
-												.then(function(answer) {
-													if (answer.managehub22 == 'No'){
-														console.log("Ok bye")
-													}
-													if (answer.managehub22 == 'Yes') {
-														require('child_process').fork('managebots.js');
-													}
-												});
-										});
-								}
-								if (answer.djssetup == 'Basic bot with a command handler') {
-									inquirer.prompt([{
-										type: 'input',
-										name: 'dirname',
-										message: 'What do you want the bot dir to be?',
-										default: 'discord-bot',
-									},
-									{
-										type: 'input',
-										name: 'token',
-										message: 'What is your bots token?',
-									},
-									{
-										type: 'input',
-										name: 'prefix',
-										message: 'What do you want your bots prefix to be?',
-										default: '!',
-									},
-									{
-										type: 'input',
-										name: 'status',
-										message: 'What do you want your bots status to be?',
-									},
-									])
-										.then(function(answer) {
-											const commandsf = 'commands';
-											const botDir = `${answer.dirname}`;
-											if (!fs.existsSync(botDir)) {
-												fs.mkdirSync(botDir);
-											}
-											fs.mkdir(
-												`${botDir}/${commandsf}`, {
-													recursive: true,
-												},
-												(err) => {
-													if (err) throw err;
-												},
-											);
-											const writeStream = fs.createWriteStream(`./${botDir}/config.json`);
-											writeStream.write('{ \n');
-											writeStream.write(`  "token": "${answer.token}", \n`);
-											writeStream.write(`  "prefix": "${answer.prefix}", \n`);
-											writeStream.write(`  "status": "${answer.status}" \n`);
-											writeStream.write('} \n');
-											writeStream.end();
-
-											fs.writeFile(`./${botDir}/index.js`, cmdhandler.index, (err) => {
-												// throws an error, you could also catch it here
-												if (err) throw err;
-
-												// success case, the file was saved
-											});
-
-											fs.writeFile(`./${botDir}/${commandsf}/hello.js`, cmdhandler.command1, (err) => {
-												// throws an error, you could also catch it here
-												if (err) throw err;
-
-												// success case, the file was saved
-											});
-
-
-											fs.writeFile(`./${botDir}/manage.js`, cmdhandler.manage1, (err) => {
-												// throws an error, you could also catch it here
-												
-											});
-											inquirer.prompt([{
-												type: 'list',
-												name: 'managehub',
-												message: 'Would you like to go to manage hub, By going here you can install needed packages!',
-												choices: ['Yes', 'No'],
-											}])
-												.then(function(answer) {
-													if (answer.managehub == 'Yes') {
-														require('child_process').fork('managebots.js');
-													}
-													if (answer.managehub == 'No') {
-														console.log('Closing');
-													}
-												});
-										});
-								}
-							});
-					}
-				});
-		}
-		if (answer.startanswer == 'Update DBH') {
-			console.log("Updating DBH")
-			inquirer.prompt([{
-				type: 'list',
-				name: 'os',
-				message: 'What is your OS',
-				choices: ['Windows','Linux/Mac']
-			}])
-			.then(function(answer){
-				if (answer.os == 'Windows'){
-					console.log("Updating DBH, This may take awhile")
-					shell.exec('npm i -g discord-bot-hub')
-					console.log("Finished install")
-				}
-				else if (answer.os == 'Linux/Mac'){
-					console.log("Updating DBH, This may take awhile")
-					shell.exec('sudo npm i -g discord-bot-hub')
-					console.log("Finished updating")
-				}
-			})
-		}
+	}).on('close',function(){
+	  console.log('bye')
+	  resolve(42) // this is the final result of the function
 	});
+  })
+}
+
+async function run() {
+  try {
+	let replResult = await replDemo()
+	console.log('repl result:', replResult)
+
+  } catch(e) {
+	console.log('failed:', e)
+  }
+}
+
+run()
+`;
+
+exports.manage2 = `
+const readline = require('readline');
+const shell = require("shelljs")
+const prompt = require('prompt-sync')();
+
+shell.exec("clear")
+console.log("You can type help to find out the commands")
+
+function replDemo() {
+  return new Promise(function(resolve, reject) {
+	let rl = readline.createInterface(process.stdin, process.stdout)
+	rl.setPrompt('ready> ')
+	rl.prompt();
+	rl.on('line', function(line) {
+	  if (line === "exit" || line === "quit" || line == 'q') {
+		rl.close()
+		return // bail here, so rl.prompt() isn't called again
+	  }
+
+	  if (line === "help" || line === '?') {
+		console.log(\`[vsc] Open vsc \n[start] Start The Bot \n[update] Update Needed Packages \n[install] Install Packages \n[needed] Run this to install needed packages \n [exit] this will exit this script\`)
+	  } else if (line === "start") {
+		shell.exec("node index.js")
+		rl.close()
+	  } else if (line === "vsc") {
+		shell.exec("code .")
+	  } else if (line === "update"){
+		console.log("Updating packages")
+		shell.exec("npm i")
+	  } else if (line === "install"){
+		console.log("What package do you wish to install")
+		var package = prompt()
+		shell.exec(\`npm i \${package}\`)
+		console.log("Installed fs")
+	  } else if (line === "needed"){
+		console.log("Installing the packages")
+		shell.exec("npm i discord.js")
+		console.log("Installed Discord.js")
+		shell.exec("npm i fs")
+		console.log("Installed fs")
+	  } else {
+		console.log(\`Unknown command: "$\{line}"\`)
+	  }
+	  rl.prompt()
+
+	}).on('close',function(){
+	  console.log('bye')
+	  resolve(42) // this is the final result of the function
+	});
+  })
+}
+
+async function run
+	let replResult = await replDemo()
+	console.log('repl result:', replResult)
+
+  } catch(e) {
+	console.log('failed:', e)
+  }
+}
+
+run()
+`;
+
+exports.managefile = `
+const prompt = require("prompt-sync")();
+var shell = require("shelljs");
+shell.config.verbose = false;
+
+shell.exec('clear')
+shell.exec(
+  "ls -I node_modules -I start.js -I managebots.js -I package.json -I package-lock.json -I app.js -I README.md -I LICENSE"
+);
+const cdfile = prompt("What bot would you like to manage: ");
+
+shell.cd(\`./\${cdfile}\`);
+
+require("child_process").fork("manage.js");
+
+`;
+
+exports.command1 = `
+const Discord = require("discord.js")
+module.exports.run = async (client, message, args) => {
+    message.channel.send("Hellooooo")
+}
+module.exports.help = {
+    name: "hello"
+}
+`;
+
+exports.index = `
+let Discord = require("discord.js");
+let fs = require("fs")
+let client = new Discord.Client();
+client.commands = new Discord.Collection();
+const config = require("./config.json")
+client.token = config.token;
+client.status = config.status;
+fs.readdir("./commands/", (err, files) => {
+    if (err) console.log(err)
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if(jsfile.length <= 0){
+        console.log("No commands found...")
+        return;
+    }
+    jsfile.forEach((f, i) => {
+        let props = require(\`./commands/\${f}\`)
+        console.log(\`\${f} loaded\`)
+        client.commands.set(props.help.name, props);
+    })
+})
+client.on('message', async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+    let prefix = config.prefix;
+    let messageArray = message.content.split(" ")
+    let cmd = messageArray[0]
+    let args = messageArray.slice(1)
+    let commandfile = client.commands.get(cmd.slice(prefix.length));
+    if(commandfile) commandfile.run(client, message, args);
+})
+client.on("ready", () =>{
+    client.user.setPresence({
+        status: 'online',
+        activity: {
+            name: config.status,
+            type: 'WATCHING',
+            url: 'https://www.twitch.tv/'
+        }
+    })
+ });
+client.login(client.token)
+
+`;
